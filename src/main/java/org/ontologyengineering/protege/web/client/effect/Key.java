270ff2d30794acb1925061fbe7c48957bc9df7a7
Eric Kow <eric.kow@gmail.com>
2014-03-28 17:25:51 +0000

    Bugfix? base Key equality on object ref equality
    I'm a bit confused about whether we need this or not, to be honest

--- src/main/java/org/ontologyengineering/protege/web/client/effect/Key.java
+++ src/main/java/org/ontologyengineering/protege/web/client/effect/Key.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import static java.lang.System.identityHashCode;
+
 /**
  * VisualEffect key: tuple of object it applies on, and the attribute
  */
@@ -15,6 +17,26 @@ public class Key {
 
     public String toString() {
         return ("[" + object.getClass() + "@" + object.hashCode() + "] " + attribute);
+    }
+
+    public boolean equals(Object obj) {
+        if (obj.getClass() != this.getClass()) {
+            return false;
+        } else {
+            // for keys, the notion of equality used means "points to the same object"
+            // (plus attribute equality as normal)
+            Key other = (Key)obj;
+            return (this.object == other.object && this.attribute.equals(other.attribute));
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + identityHashCode(object);
+        result = prime * result + attribute.hashCode();
+        return result;
     }
 
 }
