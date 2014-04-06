709ed09e9b6759d7e4da78f058adb7e96fc4516a
Eric Kow <eric.kow@gmail.com>
2014-04-06 12:01:05 +0100

    WIP: Reduce the information saved in diagram

--- src/main/java/org/ontologyengineering/protege/web/client/ui/conceptdiagram/Diagram.java
+++ src/main/java/org/ontologyengineering/protege/web/client/ui/conceptdiagram/Diagram.java
@@ -1,8 +1,10 @@
 package org.ontologyengineering.protege.web.client.ui.conceptdiagram;
 
+import com.google.common.base.Optional;
 import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
 import com.google.common.collect.HashMultimap;
+import com.google.gwt.core.client.GWT;
 import lombok.NonNull;
 import org.ontologyengineering.protege.web.client.ui.pattern.CurveCore;
 import org.semanticweb.owlapi.model.IRI;
@@ -17,33 +19,37 @@ import java.util.Map;
  * Just a thin GWT-serializable wrapper around RealDiagram
  * By rights, RealDiagram should be GWT-serializable, but at
  * the time of this writing, I get runtime errors
+ *
+ * Note that the structures here are somewhat reduced with respect to
+ * the original. This is with duplicate sources of information removed
  **/
 public class Diagram implements Serializable {
     private HashMap<IRI, IRI> immediateParents;
-    private Map<IRI, ArrayList<CurveCore>> namedCurves;
-    private Map<IRI, String> names;
+    private ArrayList<CurveCore> namedCurves;
 
     public Diagram() {
         this.immediateParents = new HashMap<IRI, IRI>();
-        this.namedCurves = new HashMap<IRI, ArrayList<CurveCore>>();
-        this.names = new HashMap<IRI, String>();
+        this.namedCurves = new ArrayList<CurveCore>();
     }
 
     public Diagram(RealDiagram diagram) {
         this.immediateParents = diagram.immediateParents;
-        this.namedCurves =  new HashMap<IRI, ArrayList<CurveCore>>();
-        for (Map.Entry<IRI, Collection<CurveCore>> entry : diagram.namedCurves.asMap().entrySet()) {
-            namedCurves.put(entry.getKey(), new ArrayList<CurveCore>(entry.getValue()));
-        }
-        this.names = new HashMap<IRI, String>(diagram.names);
+        this.namedCurves = new ArrayList<CurveCore>(diagram.namedCurves.values());
     }
 
     public static RealDiagram unpack(@NonNull final Diagram diagram) {
         RealDiagram result = new RealDiagram();
         result.immediateParents = diagram.immediateParents;
-        result.names = HashBiMap.create(diagram.names);
-        for (Map.Entry<IRI, ArrayList<CurveCore>> entry : diagram.namedCurves.entrySet()) {
-            result.namedCurves.putAll(entry.getKey(), entry.getValue());
+        result.names = HashBiMap.create();
+        for (CurveCore curveCore : diagram.namedCurves) {
+            if ((! curveCore.getIri().isPresent()) || (! curveCore.getLabel().isPresent())) {
+                final String msg = "Don't know how to unpack diagrams with anonymous curves";
+                throw new IllegalArgumentException(msg);
+            }
+            final IRI iri = curveCore.getIri().get();
+            final String name = curveCore.getLabel().get();
+            result.names.put(iri, name);
+            result.namedCurves.put(iri, curveCore);
         }
         return result;
     }
